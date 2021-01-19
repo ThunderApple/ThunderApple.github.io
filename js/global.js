841666a3f920f24bb94ec3b1a9e293d2518de1bd
Aslakjs <aslakjs@stud.ntnu.no>
2021-01-19 11:50:05 +0100

    changes to footer size + comment-section size

--- js/global.js
+++ js/global.js
@@ -1,2 +1,8 @@
 var cYear = (new Date()).getFullYear();
 document.getElementById("copyYear").innerHTML = cYear;
+
+paypal.Button.render({
+    style: {
+        size: 'small'
+    }
+}, '#paypal-button-container');
