dfe284b1262ddbcd754aca1a91231631969e0c4a
Aslakjs <aslakjs@stud.ntnu.no>
2021-01-20 11:11:13 +0100

    moved some <scrpts> to the page where it's used.

--- js/global.js
+++ js/global.js
@@ -1,8 +1,2 @@
 var cYear = (new Date()).getFullYear();
 document.getElementById("copyYear").innerHTML = cYear;
-
-paypal.Button.render({
-    style: {
-        size: 'small'
-    }
-}, '#paypal-button-container');
