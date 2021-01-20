6f964afcdf10314d14c6b125a7af51218688ff02
Aslakjs <aslakjs@stud.ntnu.no>
2021-01-20 15:36:45 +0100

    Values are now stored, no need to input multiple times...

--- js/global.js
+++ js/global.js
@@ -4,15 +4,70 @@ document.getElementById("copyYear").innerHTML = cYear;
 //------------------------------------------------------
 // GLOBAL VARIABLES
 //------------------------------------------------------
-var rat1;
-var rat2;
-var rat3;
-var rat4;
+var rat1 = 0;
+var rat2 = 0;
+var rat3 = 0;
+var rat4 = 0;
 
-var box1;
-var box2;
-var box3;
-var box4;
+var box1 = 0;
+var box2 = 0;
+var box3 = 0;
+var box4 = 0;
 
-var shouse;
-var ohouse; 
+var shouse = 0;
+var ohouse = 0; 
+
+//------------------------------------------------------
+// SET VALUES
+//------------------------------------------------------
+// Rat Traps:
+const rtReverseLookup = {
+    0  : 0,
+    2  : 1,
+    2.3: 2,
+    2.7: 3,
+    3  : 4,
+    3.3: 5,
+    3.7: 6,
+    4  : 7,
+    4.3: 8,
+    4.7: 9,
+    5  : 10,
+    6  : 11,
+    7  : 12,
+    8  : 13,
+}
+// Box of Plant
+const bpReverseLookup = {
+    0   : 0,
+    1   : 1,
+    1.25: 2,
+    1.5 : 3,
+    1.75: 4,
+    2   : 5,
+    2.25: 6,
+    2.5 : 7,
+    2.75: 8,
+    3   : 9,
+    3.25: 10,
+    3.5 : 11,
+    3.75: 12,
+    4   : 13,
+}
+// BoosterHouse
+const boostReverseLookup = {
+    0 : 0,
+    5 : 1,
+    7 : 2,
+    10: 3,
+    12: 4,
+    15: 5,
+    18: 6,
+    20: 7,
+    23: 8,
+    25: 9,
+    28: 10,
+    32: 11,
+    36: 12,
+    40: 13,
+}
---
+++ js/keepValues.js
@@ -0,0 +1,35 @@
+// Rat Trap Updates
+if (rat1 != 0) {
+    document.getElementById("rt1").selectedIndex = rtReverseLookup[rat1];
+}
+if (rat2 != 0) {
+    document.getElementById("rt2").selectedIndex = rtReverseLookup[rat2];
+}
+if (rat3 != 0) {
+    document.getElementById("rt3").selectedIndex = rtReverseLookup[rat3];
+}
+if (rat4 != 0) {
+    document.getElementById("rt4").selectedIndex = rtReverseLookup[rat4];
+}
+
+// Box of Plant Updates
+if (box1 != 0) {
+    document.getElementById("bp1").selectedIndex = bpReverseLookup[box1];
+}
+if (box2 != 0) {
+    document.getElementById("bp2").selectedIndex = bpReverseLookup[box2];
+}
+if (box3 != 0) {
+    document.getElementById("bp3").selectedIndex = bpReverseLookup[box3];
+}
+if (box4 != 0) {
+    document.getElementById("bp4").selectedIndex = bpReverseLookup[box4];
+}
+
+// Boosters
+if (shouse != 0) {
+    document.getElementById("sh").selectedIndex = boostReverseLookup[shouse];
+}
+if (ohouse != 0) {
+    document.getElementById("oh").selectedIndex = boostReverseLookup[ohouse];
+}
