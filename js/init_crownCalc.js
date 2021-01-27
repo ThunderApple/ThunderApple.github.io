40cb978e4c3bffa249a4ecc71697f733aaed1f6a
Aslakjs <aslakjs@stud.ntnu.no>
2021-01-27 13:36:16 +0100

    valuta exchange api working, crown page added

--- js/init_crownCalc.js
+++ js/init_crownCalc.js
@@ -6,41 +6,16 @@ document.getElementById("hNOK").innerHTML = hNOK;
 document.getElementById("wNOK").innerHTML = wNOK;
 document.getElementById("sNOK").innerHTML = sNOK;
 
-var vNOK = 8.56;
-document.getElementById("vNOK").innerHTML = vNOK;
-document.getElementById("vNOK").innerHTML = getNOKvalue();
+var vNOK = 0;
 
-savedValuta = "/support/rates.txt";
-editFile = fopen(savedValuta, 3);// opens the file for writing
-fwrite(file, vNOK);// str is the content that is to be written into the file.
-
-
-// execute the conversion using the "convert" endpoint:
-function getNOKvalue() {
-    // set endpoint and your access key
-    endpoint = 'live';
-    access_key = 'efb917cc898c936b10a4e64f6a7d734f';
-
-    // define from currency, to currency, and amount
-    source = 'USD';
-    currencies = 'NOK';
-    format = '1';
-
-    $.ajax({
-        url: 'http://apilayer.net/api/live?access_key=efb917cc898c936b10a4e64f6a7d734f&currencies=NOK&source=USD&format=1',
-        //url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key + '&currencies=' + currencies + '&source=' + source + '&format=' + format,   
-        dataType: 'jsonp',
-    
-        success: function(json) {
-            // access the conversion result in json.result
-            document.getElementById("vNOK").innerHTML = json.quotes.USDNOK;
-        }
+try {
+    $.getJSON('https://api.exchangeratesapi.io/latest?base=USD&symbols=NOK', 
+    function(json) {
+        vNOK = json.rates.NOK;
+        vNOK = vNOK.toFixed(2);
+        document.getElementById("vNOK").innerHTML = "= " + vNOK;
     });
 }
-
-
-
-$.getJSON('http://apilayer.net/api/live?access_key=efb917cc898c936b10a4e64f6a7d734f&currencies=NOK&source=USD&format=1', 
-function(json) {
-    document.getElementById("vNOK").innerHTML = json.quotes.USDNOK;
-});
+catch {
+    document.getElementById("vNOK").innerHTML = '&asymp; 8.56';
+}
