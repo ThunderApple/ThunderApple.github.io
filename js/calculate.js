2d11b6b4d8b9c11442644291af07f771cc6a7643
Aslakjs <aslakjs@stud.ntnu.no>
2021-02-24 11:47:47 +0100

    started moving time-calc into food-calc

--- js/calculate.js
+++ js/calculate.js
@@ -2,55 +2,7 @@
 // FUNCTIONS
 //----------------------------------------------------------
 function calcFood() {
-    
-    // Table 1
-    rat1 = (document.getElementById("rt1").value)/1; 
-    rat2 = (document.getElementById("rt2").value)/1; 
-    rat3 = (document.getElementById("rt3").value)/1; 
-    rat4 = (document.getElementById("rt4").value)/1; 
-
-    box1 = (document.getElementById("bp1").value)/1; 
-    box2 = (document.getElementById("bp2").value)/1; 
-    box3 = (document.getElementById("bp3").value)/1; 
-    box4 = (document.getElementById("bp4").value)/1; 
-
-    shouse = (document.getElementById("sh").value)/1; 
-    ohouse = (document.getElementById("oh").value)/1; 
-
-    hRat = rat1 + rat2 + rat3 + rat4;
-    hRat = hRat + ((hRat*shouse)/100);
-
-    hBox = box1 + box2 + box3 + box4;
-    hBox = hBox + ((hBox*ohouse)/100);
-
-    wRat = hRat*24*7;
-    wBox = hBox*24*7;
-
-    document.getElementById("hRt").innerHTML = hRat.toFixed(2);
-    document.getElementById("hBp").innerHTML = hBox.toFixed(2);
-
-    document.getElementById("wRt").innerHTML = wRat.toFixed(2);
-    document.getElementById("wBp").innerHTML = wBox.toFixed(2);
-
-    // Table 2
-    tFood = wRat+wBox;
-    nFood = gFood-tFood;
-
-    document.getElementById("tFood").innerHTML = tFood.toFixed(2);
-    if (nFood<=0) {
-        nFood = "Goal!"
-        document.getElementById("nFood").innerHTML = nFood;
-    }
-    else {
-        document.getElementById("nFood").innerHTML = nFood.toFixed(2);
-    }
-}
-
-
-function calcTime() {
-    pDays = 0;
-    pHour = 0;
-
+    // Table 1 - INPUT
     rat1 = (document.getElementById("rt1").value)/1; 
     rat2 = (document.getElementById("rt2").value)/1; 
     rat3 = (document.getElementById("rt3").value)/1; 
@@ -68,12 +20,21 @@ function calcTime() {
     iFood = (document.getElementById('iFood').value)/1;
 
 
+    // Table 1 - CALC
     hRat = rat1 + rat2 + rat3 + rat4;
     hRat = hRat + ((hRat*shouse)/100);
 
     hBox = box1 + box2 + box3 + box4;
     hBox = hBox + ((hBox*ohouse)/100);
 
+    wRat = hRat*24*7;
+    wBox = hBox*24*7;
+
+    document.getElementById("hRt").innerHTML = hRat.toFixed(2);
+    document.getElementById("hBp").innerHTML = hBox.toFixed(2);
+
+    document.getElementById("wRt").innerHTML = wRat.toFixed(2);
+    document.getElementById("wBp").innerHTML = wBox.toFixed(2);
 
     hProduction = hRat + hBox;
     document.getElementById("hProd").innerHTML = hProduction.toFixed(2);
@@ -90,7 +51,22 @@ function calcTime() {
         pTime = pDays.toString() + ":" + pHour.toString();
         document.getElementById("pTime").innerHTML = pTime;
     }
+
+    // Table 2 - CALC
+    tFood = wRat+wBox;
+    nFood = gFood-tFood;
+
+    document.getElementById("tFood").innerHTML = tFood.toFixed(2);
+    if (nFood<=0) {
+        nFood = "Goal!"
+        document.getElementById("nFood").innerHTML = nFood;
+    }
+    else {
+        document.getElementById("nFood").innerHTML = nFood.toFixed(2);
+    }
 }
+
+
 
 
 function calcNOK() {
--- js/init_foodCalc.js
+++ js/init_foodCalc.js
@@ -6,6 +6,14 @@ var wBox = 0;
 var tFood = 0;
 var gFood = 10000;
 var nFood = gFood-tFood;
+var iFood = 0;
+var mFood = gFood-iFood;
+
+var pDays = 0;
+var pHour = 0;
+
+var pTime = "∞";
+var hProduction = 0;
 
 document.getElementById("hRt").innerHTML = hRat;
 document.getElementById("hBp").innerHTML = hBox;
@@ -16,3 +24,7 @@ document.getElementById("wBp").innerHTML = wBox;
 document.getElementById("tFood").innerHTML = tFood.toFixed(2);
 document.getElementById("gFood").innerHTML = gFood.toFixed(2);
 document.getElementById("nFood").innerHTML = nFood.toFixed(2);
+document.getElementById("mFood").innerHTML = mFood.toFixed(2);
+
+document.getElementById("pTime").innerHTML = pTime;
+document.getElementById("hProd").innerHTML = hProduction.toFixed(2);
